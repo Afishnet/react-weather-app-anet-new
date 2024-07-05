117baa42a0152354aa14db4a520f35a06878890b
Afishnet <jakesovaan@gmail.com>
2024-07-05 15:06:59 +1000

    HTML

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <Weather></Weather>
+    </div>
+  );
 }
