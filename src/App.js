160553dd8aaf774e163568ce581cc0cd47869ec9
Afishnet <jakesovaan@gmail.com>
2024-07-05 15:09:24 +1000

    footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ export default function App() {
   return (
     <div className="App">
       <Weather></Weather>
+      <footer>
+        Coded with <span class="emoji">🤍</span> by{" "}
+        <a href="https://github.com/Afishnet" target="_blank">
+          Anet Jakesova
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/Afishnet?tab=repositories" target="_blank">
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://app.netlify.com/teams/afishnet/overview"
+          target="_blank"
+        >
+          {" "}
+          hosted on Netlify.
+        </a>
+      </footer>
     </div>
   );
 }
